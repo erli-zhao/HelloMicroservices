9e3f4cbaae596408ed35025437bd1ac80d3374d8
赵利杰 <zhaolj@mail.jj.cn>
2020-07-15 18:55:07 +0800

    添加项目文件。

---
+++ HelloMicroservices/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Nancy.Owin;
+namespace HelloMicroservices
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseOwin(buildFunc =>
+            {
+                buildFunc(next => en =>
+                {
+                    System.Console.WriteLine("Got Request");
+                    return next(en);
+                });
+                buildFunc.UseNancy();
+            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
